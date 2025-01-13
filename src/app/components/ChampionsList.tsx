e3a22791dfdb1629e232a674821306efc079d19a
fyang151 <fyang151@gmail.com>
2025-01-12 21:11:50 -0800

    WIP useListGame logic

--- src/app/components/ChampionsList.tsx
+++ src/app/components/ChampionsList.tsx
@@ -1,7 +1,33 @@
-
+"use client";
+import { useState } from "react";
+import { useListGame } from "../hooks/useListGame";
 
 const ChampionList = () => {
-  return <div>hello</div>;
+  const { pixelatedChampions, loading } = useListGame({
+    xPixels: 4,
+    yPixels: 4,
+    isGrayScale: false,
+  });
+
+  // meow is just to test what happens on rerender
+  const [_, meow] = useState(null);
+
+  return (
+    <div>
+      <button onClick={() => meow(null)}>hi</button>
+      {loading ? (
+        <div>loading...</div>
+      ) : (
+        <ul>
+          {pixelatedChampions?.map((pixelatedChampion) => (
+            <li key={pixelatedChampion}>
+              <img src={pixelatedChampion} />
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
 };
 
 export default ChampionList;
--- src/app/hooks/useListGame.ts
+++ src/app/hooks/useListGame.ts
@@ -5,10 +5,39 @@ import { useState, useEffect } from "react";
 import championIcons from "../data/championIcons";
 import champions from "../data/champions.json";
 
+import { getPixelatedImage } from "../utils/pixelateImage";
 import { useGameProps } from "../types/champion";
 
-export const useRandomGame = ({
+export const useListGame = ({
   xPixels,
   yPixels,
   isGrayScale,
-}: useGameProps) => {};
+}: useGameProps) => {
+  const [pixelatedChampions, setPixelatedChampions] = useState<string[]>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const iconItemList = Object.values(championIcons);
+
+    const addPixelatedChampion = (icon: string) => {
+      return getPixelatedImage(icon, xPixels, yPixels, isGrayScale);
+    };
+
+    Promise.all(
+      iconItemList.map((iconItem) => addPixelatedChampion(iconItem.default.src))
+    ).then((pixelatedImages) => {
+      setPixelatedChampions(pixelatedImages);
+      setLoading(false);
+    });
+  });
+
+  return { pixelatedChampions, loading };
+};
+
+const shuffle = <T>(arr: T[]) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+};
