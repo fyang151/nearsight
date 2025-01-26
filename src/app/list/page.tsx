967c9c1bb36b69ded1434d03c592fd2bc2d0c8e9
fyang151 <fyang151@gmail.com>
2025-01-26 00:49:23 -0800

    aha this will work now

--- src/app/list/page.tsx
+++ src/app/list/page.tsx
@@ -1,17 +1,27 @@
 import Header from "../components/Header";
 import ChampionList from "../components/ChampionsList";
 
-// i dont wanna fix this build error
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export default function Home({ searchParams }: { searchParams: any }) {
+type SearchParams = {
+  xPixels?: string;
+  yPixels?: string;
+  isGrayScale?: string;
+};
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<SearchParams>;
+}) {
+  const params = await searchParams;
+
   return (
     <>
       <Header />
       <div className="flex justify-center items-center h-full">
         <ChampionList
-          xPixelsFromUrl={Number(searchParams.xPixels)}
-          yPixelsFromUrl={Number(searchParams.yPixels)}
-          isGrayScaleFromUrl={Boolean(Number(searchParams.isGrayScale))}
+          xPixelsFromUrl={Number(params.xPixels)}
+          yPixelsFromUrl={Number(params.yPixels)}
+          isGrayScaleFromUrl={Boolean(Number(params.isGrayScale))}
         />
       </div>
     </>
