bfd41afcfdb45e7288718d4ebf3061613b8df087
fyang151 <fyang151@gmail.com>
2025-01-26 21:28:59 -0800

    fix build errors

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 const Header = ({ currentPage }: { currentPage?: string }) => {
   return (
     <div className="flex items-center justify-between p-2">
       <h1 className="hidden sm:block text-3xl">Nearsight.cc</h1>
       <div>
-        <a
+        <Link
           href="/"
           className={`text-lg mr-4 ${currentPage === "random" ? "italic" : ""}`}
         >
           Random
-        </a>
+        </Link>
         <a
           href="/list"
           className={`text-lg mr-4 ${currentPage === "list" ? "italic" : ""}`}
